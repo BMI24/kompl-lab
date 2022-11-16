b9acdf279c0f290a3d875aa967a8f0134844dfdf
BMI24 <mitgliedschaftfo@gmx.de>
2022-11-16 13:53:26 +0100

    finish week2

--- week2/solution.py
+++ week2/solution.py
@@ -21,22 +21,21 @@ lines = None
 with open(args.filename) as file:
     lines = [line.rstrip().lstrip() for line in file]
 
+lines = [l for l in lines if not l.startswith('#') and l != '']
+
 state_count, tape_count, alphabet_size, transitions_count = [int(s) for s in lines[0].split(' ')]
 for line in lines[1:]:
-    if line.startswith('#'):
-        continue
-    
-    linesplit = line.split(',')
+    linesplit = [l.rstrip().lstrip() for l in  line.split(',')]
     q_old = linesplit[0]
     s_old = tuple(linesplit[1:1+tape_count])
-    q_new = linesplit[2+tape_count]
-    s_new = tuple(linesplit[3+tape_count:3+2*tape_count])
-    movement = movement_to_offset[linesplit[-1]]
+    q_new = linesplit[1+tape_count]
+    s_new = tuple(linesplit[2+tape_count:2+2*tape_count])
+    movement = tuple((movement_to_offset[m] for m in linesplit[2+2*tape_count:]))
     transitions[(q_old, s_old)] = (q_new, s_new, movement)
 
-tape_content: list[list[str]] = [list("S") if i > 0 else list('S'+ args.input) for i in range(tape_count)]
+tape_content: list[list[str]] = [list("S_") if i > 0 else list('S'+ args.input) for i in range(tape_count)]
 head_pos: list[int] = [1 for _ in range(tape_count)]
-state: str = "0"
+state: str = '0'
 
 def print_info():
     print('-----------------------------')
@@ -47,11 +46,13 @@ def print_info():
     print('Zustand:', state)
     print('-----------------------------')
 
+steps = 0
 while state not in halt_states:
     if args.verbose:
         print_info()
     
-    transition : Tuple[str, Tuple[str], Tuple[int]] = transitions.get(state, tuple([tape_content[i][head_pos[i]] for i in range(tape_count)]), None)
+    steps += 1
+    transition : Tuple[str, Tuple[str], Tuple[int]] | None = transitions.get((state, tuple([tape_content[i][head_pos[i]] for i in range(tape_count)])), None)
     if transition is None:
         state = "nein"
     else:
@@ -60,11 +61,18 @@ while state not in halt_states:
 
         for i in range(tape_count):
             tape_content[i][head_pos[i]] = s_new[i]
-            head_pos[i] += movement_to_offset[movement]
+            head_pos[i] += movement[i]
 
             if head_pos[i] < 0:
                 state = "nein"
             elif head_pos[i] >= len(tape_content[i]):
                 tape_content[i].append('_')
 
-print_info()
+if args.verbose:
+    print_info()
+else:
+    print('Ergebnis:')
+    result = ''.join(tape_content[-1])
+    print(result.rstrip('_')[1:])
+    print('Zustand:', state)
+    print('Schritte:', steps)
