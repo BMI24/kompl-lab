0479c75aee906e6befed832ecbef35d574bd44bd
BMI24 <mitgliedschaftfo@gmx.de>
2023-02-08 12:02:06 +0100

    add solution to week10

---
+++ week10/millerrabin.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from argparse import ArgumentParser
+from typing import List, Set, Tuple
+import numpy as np
+
+def miller_rabin(n : int, k : int) -> bool:
+    assert(n%2 != 0)
+
+    d = n
+    s = 1
+    while d % 2 == 0:
+        d = d // 2
+        s *= 2
+
+    for _ in range(k):
+        a = np.random.randint(2, n-1)
+        x = pow(a, d, n)
+        if x == 1:
+            return True
+        for _ in range(s):
+            if x % n == n - 1:
+                return True
+            else:
+                x = pow(x, 2, n)
+    return False
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('n')
+    parser.add_argument('k')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+
+    n = int(args.n)
+    k = int(args.k)
+    print(miller_rabin(n, k))
---
+++ week10/test.py
@@ -0,0 +1,18 @@
+from millerrabin import miller_rabin
+import numpy as np
+from pretty_confusion_matrix import pp_matrix
+from tqdm import tqdm
+import pandas as pd
+
+primes = set(np.genfromtxt('primes.csv', delimiter=',', dtype=int)[1:, 1])
+
+confusion_matrix = np.array([[0, 0], [0, 0]])
+
+for i in tqdm(range(5, int(1e3), 2)):
+    reality_prime = i in primes
+    prediction_prime = miller_rabin(i, 20)
+
+    confusion_matrix[int(reality_prime), int(prediction_prime)] += 1
+
+cmap = 'PuRd'
+pp_matrix(pd.DataFrame(confusion_matrix), cmap=cmap)
